e78a0cd368c54a2786a6e13c874069e758b1bf55
Й <>
2020-10-06 13:16:02 +0300

    1
    1

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+#include <tchar.h>
+using namespace std;
+
+int main()
+{ 
+    setlocale(LC_ALL, "Russian");
+    long int a(9), b(5), c(0);
+    c = a + b;
+    cout << "Введите 1-е число" << endl;
+    cin >> a;
+    cout << "Введите 2-е число" << endl;
+    cin >> b;
+    _asm
+    {
+        mov eax, a
+        add eax, b
+        mov c, eax
+    }
+    cout << "Ответ: " << c << endl;
+    system("PAUSE");
+    return 0;
+}
+
